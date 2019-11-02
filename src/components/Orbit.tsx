3ea3d0cbce2ae63c7d1d6080ce20f869f9cff8f7
monedula-uni <sh318@st-andrews.ac.uk>
2019-11-02 17:09:54 +0000

    added updateCanvas and dummy thicc consts

--- src/components/Orbit.tsx
+++ src/components/Orbit.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { convertToView } from '@ionic/core/dist/types/components/nav/view-controller';
 
+function ellipse(props:any){
+    const {ctx, x, y, a, b, r} = props;
+    ctx.beginPath()
+    ctx.ellipse(x,y,a,b,r,0,2 * Math.PI);
+    ctx.stroke();
 
+}
 
 //creates orbit ellipse on canvas
 class Orbit extends React.Component {
@@ -12,18 +18,28 @@ class Orbit extends React.Component {
     }
 
     componentDidMount() {
+        this.updateCanvas();
+      }
+      
+    componentDidUpdate(){
+        this.updateCanvas();
+    }
+    
+      updateCanvas(){
         const canvas = this.orbit.current;
         const ctx = canvas.getContext('2d');
         //ctx.fillRect(0, 0, canvas.width, canvas.height);
         //third is major axis, fourth is minor axis
         //fifth is rotation in rads
         //calulcate b using eccentricity - will have to be scaled up to match somhow
-        const b=2.9;
+
+        const x=150;
+        const y=150;
+        const a=60;
+        const b=30;
         const rotation=0;
-        ctx.beginPath()
-        ctx.ellipse(100,150,4.5,b,rotation,0,2 * Math.PI);
-        ctx.stroke();
-      }    
+        ellipse({ctx,x,y,a,b,rotation});
+      }
 
     render() {
 /*         var orbitStyle = {

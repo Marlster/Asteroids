56683ef83785e76195a536b0e0cca6544d79696a
Marlster <marley7898@gmail.com>
2019-11-02 16:43:41 +0000

    button

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,32 +1,61 @@
 import React, { Component } from 'react'
+import Orbit from './Orbit';
 class Map extends React.Component<{bgColor: string, e: number, a: number}>{
 
     state = {
         orbits: {
-            someorbit: 2
-        }
+            eccentricity: 2,
+            a: 1
+        },
+        text: "PRESS TO LAUNCH", 
+        launched: false
     }
 
     handleClick () {
-        console.log('Success!')
+        this.setState({text: "GET READY"})
+        var _this = this;
+        setTimeout(function(){
+            _this.setState({text: "TO"})
+        },2000);
+        setTimeout(function(){
+            _this.setState({text: "LAUNCH"})
+        },4000);
+        setTimeout(function(){
+            _this.setState({text: "3"})
+        },6000);
+        setTimeout(function(){
+            _this.setState({text: "2"})
+        },7000);
+        setTimeout(function(){
+            _this.setState({text: "1"})
+        },8000);
+        setTimeout(function(){
+            _this.setState({text: "BLASTOFF!!!!"})
+        },9000);
+
     }
     componentDidMount = async () => {
-        let response = await fetch("http://someurl:8000/ENDPOINT", {
+        let response = await fetch("http://someurl:3000/ENDPOINT", {
             method: 'GET',
             credentials: "same-origin",
-            mode: "same-origin",
-        }
-        );
-        this.setState({onlineUsers: response});
+            mode: "same-origin",    
+        });
+        this.setState({orbits: response});
     }
     render () {
-        return (
-        <div className='button__container'>
-            <button className='button' onClick={this.handleClick}>
-            Click Me
-            </button>
-        </div>
-        )
+        if (this.state.launched) {
+            return (
+                <Orbit bgColor="blue" orbits={this.state.orbits}/>
+            )
+        } else {
+            return (
+                <div className='button__container'>
+                    <button className='button' onClick={this.handleClick}>
+                        {this.state.text}
+                    </button>
+                </div>
+            )
+        }
     }
 }
 

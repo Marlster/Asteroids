f151f2c45184a3d49748b131214b5834687caf44
monedula-uni <sh318@st-andrews.ac.uk>
2019-11-02 16:58:37 +0000

    canvas

--- src/components/Orbit.tsx
+++ src/components/Orbit.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
-import Konva from 'konva';
+import { convertToView } from '@ionic/core/dist/types/components/nav/view-controller';
 
-class Orbit extends React.Component<{bgColor: string, orbits: JSON}>{
+
+
+//creates orbit ellipse on canvas
+class Orbit extends React.Component {
+    private orbit: React.RefObject<HTMLCanvasElement>;
+    constructor (props){
+        super(props);
+        this.orbit = React.createRef();
+    }
+
+    componentDidMount() {
+        const canvas = this.orbit.current;
+        const ctx = canvas.getContext('2d');
+        //ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //third is major axis, fourth is minor axis
+        //fifth is rotation in rads
+        //calulcate b using eccentricity - will have to be scaled up to match somhow
+        const b=2.9;
+        const rotation=0;
+        ctx.beginPath()
+        ctx.ellipse(100,150,4.5,b,rotation,0,2 * Math.PI);
+        ctx.stroke();
+      }    
+
     render() {
-        var orbitStyle = {
+/*         var orbitStyle = {
             padding:10,
             margin:20,
             display:"inline-block",
             backgroundColor: this.props.bgColor,
             borderRadius: "50%",
-            transform: "scaleX(" + 2 + ")",
+            transform: "scaleX(" + this.props.e + ")",
             width:100,
             height:100,
-        };
-        return (
-            <div style={orbitStyle}>
+
+            put back in return maybe 
+                        <div style={orbitStyle}>
             </div>
+            
+        }; */
+        return (
+            <canvas ref={this.orbit} width={200} height={300}/ >
         );
     }
 };
